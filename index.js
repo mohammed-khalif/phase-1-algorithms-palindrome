f608b33e651482b6ab333d2ca7ac5286e0a9ae13
mohammed-khalif <mohamed.yusuf@student.moringaschool.com>
2023-04-11 15:10:43 -0400

    completed assignment

--- index.js
+++ index.js
@@ -4,7 +4,21 @@ function isPalindrome(word) {
 
 /* 
   Add your pseudocode here
+
 */
+function isPalindrome(str) {
+  let left = 0;
+  let right = str.length - 1;
+  while (left < right) {
+    if (str[left] !== str[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+}
+
 
 /*
   Add written explanation of your solution here
